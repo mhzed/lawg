1177254102a6c534623b095be0dca68556b8db3d
mhzed <minhongz@gmail.com>
2018-01-03 22:21:25 +0800

    simplify

--- index.ts
+++ index.ts
@@ -1,30 +1,23 @@
-/*
-  paras: what to log, if an object then it will be converted to JSON
-*/
-
-declare var process: any;
 let debug = false;
 
 if (typeof process !== 'undefined') {
   debug = process.env['NODE_DEBUG'] ? true : false;
 }
+const ts = function () {
+  let dateTs = new Date();
+  let sts = dateTs + "";
+  let sms = `${ dateTs.getMilliseconds() }`;
+  let pad = 3 - sms.length;
+  for (let i = 0, end = pad, asc = 0 <= end; asc ? i < end : i > end; asc ? i++ : i--) {
+    sms = `0${ sms }`;
+  }
+  return sts.slice(4, 24) + "." + sms + sts.slice(24, 33);
+}();
+const inspect = function (obj) {
+  return JSON.stringify(obj);
+};
 
-export default function (...paras) {
-
-  let ts = function () {
-    let dateTs = new Date();
-    let sts = dateTs + "";
-    let sms = `${ dateTs.getMilliseconds() }`;
-    let pad = 3 - sms.length;
-    for (let i = 0, end = pad, asc = 0 <= end; asc ? i < end : i > end; asc ? i++ : i--) {
-      sms = `0${ sms }`;
-    }
-    return sts.slice(4, 24) + "." + sms + sts.slice(24, 33);
-  }();
-  let inspect = function (obj) {
-      return JSON.stringify(obj);
-  };
-
+export default function (...paras): void {
   let msg = paras.map(para => typeof para === "object" ? inspect(para) : `${ para }`).join('');
 
   if (debug) {
@@ -38,7 +31,6 @@ export default function (...paras) {
       let [file, offsets] = [m[1], m[2]];
       return console.log(`[${ ts }](${ file }:${ offsets }) ${ msg } `);
     } else {
-
       return console.log(`[${ ts }](${ e.stack }) ${ msg } `);
     }
   } else {
